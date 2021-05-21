d8b3fd232b2e929f5d25807659ed038ed173359c
Alex <filinskihalex@gmail.com>
2021-05-21 16:05:33 +0300

    my

--- mysql.php
+++ mysql.php
@@ -1,31 +1,47 @@
 <?php
+    include ('setting.php'); //файл настроек
     
-    
-    $host = '176.53.162.84';
-    $user = '';
-    $dbname = 'test';
-    $port = '5432';
-    $pwd = '';
-    $dbconnect = pg_connect ("host = $host port=$port dbname=$dbname user=$user password=$pwd ");
-
-    if (!$dbconnect) {
-        echo 'соединение не установленно';
-    } 
+      // Запрос к базе данных 
     $result = pg_query($dbconnect, 'SELECT * FROM vw_switch'); 
     if (!$result) {
         echo 'произошла ошибка';
     }
-    
+
+    // Проходим циклом по result  вытаскиваем данные из каждой строки запроса
+    // print_r($result);
     while ($row = pg_fetch_assoc($result)) {
-        $name = $row['name'];
+        $name = $row['switch_name'];
         $location = $row['location'];
         $ip = $row['ip'];
-        $mask = $row['mask'];
-         // echo $row['first_name']."<span>--</span>".$row['last_name'].$row['age']."<br/>"; -->
-        Echo $name, $location,$ip,$mask."<br/>";
-     }
-    // $table_name = 'movie';
-    // $arrayName = array('1' =>"5" ,'2'=>'Зеленая миля', '3'=>"2005",'4'=>"3",'1'=>"1" );
+        $model = $row['model'];
+        // echo $row['first_name']."<span>--</span>".$row['last_name'].$row['age']."<br/>"; -->
+        Echo 'имя'."<br>".$name, $location,$ip,$model."<br/>";
+    }
+
+    // pg_close($dbconnect); // закрываем соединение с базой после прохода цикла 
+
+    $row = pg_fetch_assoc($result);
+    foreach ($row as $key => $value) {
+        echo $key,$value.'</br>' ;
+        # code...
+    }
+
+
+    $result = pg_query($dbconnect, 'SELECT * FROM vw_switch_port'); 
+    if (!$result) {
+        echo 'произошла ошибка';
+    }
+    
+    $row = pg_fetch_assoc($result);
+    foreach ($row as $key => $value) {
+        echo $key,$value.'</br>' ;
+        # code...
+    }
+
+    pg_close($dbconnect);
+    
+    // $table_name = 'switch';
+    // $arrayName = array('location_id' =>"1" ,'name'=>"Зеленая миля", 'ip_id'=>"5",'SN'=>"PM42006422",'hardware_id'=>"1",'add_date' => "now()" );
     
     //  $quere = pg_insert($dbconnect,$table_name,$arrayName );
     //  if ($quere) {
@@ -33,11 +49,30 @@
     //  }
     //  echo $arrayName;
     // phpinfo();
+
+    // $i = 1;
+
+    // while($i  <= 254) {
+    //     echo $i.'<br>';
+    //     $i + $i++;
+
+
+    // }
+
+    // $query = "delete from switch where ip_id = 5;";
+    // $query .= "delete from ip where ip_id = 5;";
+      
+
+    // $query .= "insert into ip values(5,'192.168.2.5',24);"; 
+    // $query .= "insert into switch (location_id,name,ip_id,SN,hardware_id,add_date) values  (1,'th1sds.core1.sc',5,'PM42002222',1,now() );";
     
+    // $insert = pg_query($dbconnect,$query); 
+    //     if ($query) {
+    //         echo 'Данные вставлены';
+    //     }
+    //     pg_close($dbconnect);
 
-
-
- ?>
+?>
 
 <!DOCTYPE html>
     <html lang="en">
@@ -52,14 +87,7 @@
                 имя коммутатора <?php echo $name  ?>  размещение <?php echo $location?>
             </p>
             <p>
-                год <?php while ($row = pg_fetch_assoc($result)) {
-                $name = $row['name'];
-                $location = $row['location'];
-                $ip = $row['ip'];
-                $mask = $row['mask'];
-                // echo $row['first_name']."<span>--</span>".$row['last_name'].$row['age']."<br/>"; -->
-                Echo $name, $location,$ip,$mask."<br/>";
-            }  ?>
+               
             </p>    
 
             <p></p>
